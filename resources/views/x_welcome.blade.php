ba0cc325bfa6b7d6c443cddd1270254dc26eb6a4
Johan Nordström <johan@digitalvillage.se>
2023-06-02 01:23:55 +0200

    Added welcome-page.

---
+++ resources/views/x_welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>SWAPI</title>
+
+	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+	<div class="lolz">
+		<img src="{{ asset('images/light-saber-starwars.gif') }}" alt="I wouldn't do that if I were you">
+		<p class="heart" title="FED22M 🤘🏻🎸">❤️</p>
+	</div>
+</body>
+</html>

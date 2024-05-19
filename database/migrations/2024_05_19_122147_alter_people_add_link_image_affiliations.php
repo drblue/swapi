7623d57a2e10c7d3177969a51f17d7dac3c9329c
Johan Nordström <johan@digitalvillage.se>
2024-05-19 23:12:10 +0200

    Added `wiki_link`, `image_url` and `affiliations` to `People` model. Added `image_url` to `Film` model.

---
+++ database/migrations/2024_05_19_122147_alter_people_add_link_image_affiliations.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterPeopleAddLinkImageAffiliations extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('people', function (Blueprint $table) {
+			$table->string('wiki_link')->nullable()->after('skin_color');
+			$table->string('image_url')->nullable()->after('wiki_link');
+			// affiliations is an array of strings with default to empty
+			$table->longtext('affiliations')->default("[]")->after('image_url');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('people', function (Blueprint $table) {
+			$table->dropColumn('wiki_link');
+			$table->dropColumn('image_url');
+			$table->dropColumn('affiliations');
+		});
+	}
+}
